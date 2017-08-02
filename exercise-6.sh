7b53c356854ec33f8541b9ed27149156cb12441f
Shock Liang <shockwater@gmail.com>
2017-08-02 22:54:04 +0800

    Variables and paramaters exercise

---
+++ exercise-6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+var=11
+#unset var
+#echo $var
+
+(( var2=var<10?1:0 ))
+
+# echo $var2
+
+# read input
+# echo "Type in some value"
+# read var3
+# echo $var3
+
+for var in 1 2 3
+do
+    echo $var
+done
+
