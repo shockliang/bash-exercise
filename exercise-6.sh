ef4a82194b7167a8e9999f1c63c2a41b633ea067
Shock Liang <shockwater@gmail.com>
2017-08-03 22:39:56 +0800

    Variables and paramaters exercise 2

--- exercise-6.sh
+++ exercise-6.sh
@@ -18,3 +18,33 @@ do
     echo $var
 done
 
+var3="T r a l a l  al   lalalal   la"
+echo $var3
+
+# preserved all space
+echo "$var3"
+
+#var4="test1 test2 test3"
+var4=test1\ test2\ test3\ 
+echo "$var4"
+
+# set to null
+var4=
+echo $var4
+
+var5=11 var6=22 var7=33
+echo "$var5 $var6 $var7"
+
+# += operator
+var8=
+let "var8 +=10"
+echo "$var8"
+let "var8 = var8 + 10"
+echo "$var8"
+
+# hi=`echo test`
+# using hi variable to execute command
+# hi=`ls -a`
+hi=$(ls -la)
+echo "$hi"
+
